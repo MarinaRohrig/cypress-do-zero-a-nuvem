c919dd971ac005f256d4f92fe931f728b5910757
MarinaLarissa <marina@nextar.com.br>
2025-06-04 14:40:31 -0300

    ajustes

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -7,30 +7,32 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
   })
 
-  it.only('Fill out and submit', () =>{
+  it('Fill out and submit', () =>{
     cy.fillMandatoryFields();
     cy.contains('button','Enviar').click();
+
     cy.get('.success').should('be.visible');
   })
 
   it('Error message when incorrect email', () =>{
     cy.fillMandatoryFields();
-
     cy.get('#email').clear().type('nome.sobrenome');
     cy.contains('button','Enviar').click();
+
     cy.get('.error').should('be.visible');
   })
 
   it('Fill phone with not numeric caracters', () =>{
     cy.get('#phone').type('AaBbCc$!');
+
     cy.get('#phone').should('have.value','');
   })
   it('Error message when obrigatory phone is not filled', () => {
     cy.fillMandatoryFields();
-    
     cy.get('#phone').clear();
     cy.get('#phone-checkbox').check();
     cy.contains('button','Enviar').click();
+
     cy.get('.error').should('be.visible');
   })
 
@@ -41,14 +43,23 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#lastName').should('have.value','Sobrenome');
     cy.get('#email').should('have.value','nome.sobrenome@email.com');
     cy.get('#phone').should('have.value','489999999');
-    cy.get('#firstName').clear().should('have.value','');
-    cy.get('#lastName').clear().should('have.value','');
-    cy.get('#email').clear().should('have.value','');
-    cy.get('#phone').clear().should('have.value','');
+    cy.get('#firstName')
+      .clear()
+      .should('have.value','');
+    cy.get('#lastName')
+      .clear()
+      .should('have.value','');
+    cy.get('#email')
+      .clear()
+      .should('have.value','');
+    cy.get('#phone')
+      .clear()
+      .should('have.value','');
   })
 
   it('Error message when nothing is filled', () => {
     cy.contains('button','Enviar').click();
+    
     cy.get('.error').should('be.visible');
   })
 
